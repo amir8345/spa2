11f4c67d3776193acf640d26dfd4ced6efde8e58
amir <amir@gmail.com>
2022-05-29 20:10:39 +0430

    ...

--- database_views/notification.sql
+++ database_views/notification.sql
@@ -9,7 +9,7 @@ SELECT
 ) AS user_id , 
  
  'like' AS 'event' , likes.user_id as maker_id , 
- liked_type AS event_subject , liked_id AS event_id
+ liked_type AS event_subject , liked_id AS event_id , created_at as time
  FROM likes
 
 UNION ALL
@@ -26,7 +26,7 @@ SELECT * FROM
     END
 ) AS user_id , 
 
-'comment' as 'event' , user_id as maker_id , commented_type AS event_subject, commented_id AS event_id
+'comment' as 'event' , user_id as maker_id , commented_type AS event_subject, commented_id AS event_id , created_at as time
 
  FROM comments b ) AS table1
  WHERE table1.user_id IS NOT NULL
@@ -34,18 +34,18 @@ SELECT * FROM
 UNION ALL
 
 SELECT posted_id as user_id , 'post' as 'event' , user_id as maker_id , 
-'user' as event_subject , posted_id as event_id
+'user' as event_subject , posted_id as event_id , created_at as time
 FROM posts 
 WHERE posted_type = 'user'
 
 UNION ALL
 
-SELECT receiver AS user_id , 'suggestion' AS 'event' , user_id AS maker_id , 'book' AS event_subject , book_id AS event_id
+SELECT receiver AS user_id , 'suggestion' AS 'event' , user_id AS maker_id , 'book' AS event_subject , book_id AS event_id , created_at as time
 FROM suggestions
 
 UNION ALL
 
-SELECT follower_id as user_id , 'accept' as 'event' , following_id as 'maker_id' , 'user' as 'event_subject' , following_id as 'event_id'
+SELECT follower_id as user_id , 'accept' as 'event' , following_id as 'maker_id' , 'user' as 'event_subject' , following_id as 'event_id' , created_at as time
 FROM follows
 WHERE following_type = 'user'
 AND
@@ -56,3 +56,4 @@ AND
 following_id IN (
 SELECT id FROM users where private = true
 )
+order by time
